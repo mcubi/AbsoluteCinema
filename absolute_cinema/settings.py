314ff910ec333fd4f0161c86561bcdd454356774
David <david.fernandez@colegiomiralmonte.es>
2026-02-11 19:53:04 +0100

    primera pincelada

--- absolute_cinema/settings.py
+++ absolute_cinema/settings.py
@@ -130,6 +130,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # DEFAULT PRIMARY KEY FIELD TYPE:
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
