9e8c5106a2315088d6e06df5aaf08c56cf25d461
Bea <beatriz.ferrer@colegiomiralmonte.es>
2026-04-27 10:19:37 +0200

    movie reviews

--- movies/urls.py
+++ movies/urls.py
@@ -4,5 +4,4 @@ from . import views
 urlpatterns = [
     path('', views.home_api, name='home'),
     path('pelicula/<int:movie_id>/', views.detalle_pelicula, name='detalle_pelicula'),
-    path('reproducir/<int:movie_id>/', views.reproducir_pelicula, name='reproducir_pelicula'),
 ]
