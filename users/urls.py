4861c3118fabe4abce77deefe2eed4f3afb64ed3
Bea <beatriz.ferrer@colegiomiralmonte.es>
2026-05-07 17:21:51 +0200

    implemento modo oscuro y claro

--- users/urls.py
+++ users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('register', views.register_user, name='register'),
     path('login/', views.login_user, name='log_in'),
     path('perfil/', views.mi_perfil, name='mi_perfil'),
+    path('configuracion/', views.configuracion, name='configuracion'),
 ]
