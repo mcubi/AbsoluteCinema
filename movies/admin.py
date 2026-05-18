0d432f8b708fe72bcbffb079a24ffc5fe806e451
GonTheWuz <gonzalo.deguzman@colegiomiralmonte.es>
2026-05-18 10:41:27 +0200

    i hope not to regret for this

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Review, MiLista
 
 # Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ('user', 'movie_title', 'rating', 'created_at')
+    list_filter = ('rating', 'created_at')
+    search_fields = ('user__username', 'movie_title', 'content')
+    ordering = ('-created_at',)
+
+@admin.register(MiLista)
+class MiListaAdmin(admin.ModelAdmin):
+    list_display = ('user', 'movie_title', 'added_at')
+    list_filter = ('added_at',)
+    search_fields = ('user__username', 'movie_title')
+    ordering = ('-added_at',)
