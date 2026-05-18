9850f54fa228be903c4a3b9dd40fe08a71e65b2e
Azel <marcos.mose@colegiomiralmonte.es>
2026-05-18 07:30:15 +0200

    user - urls update

--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name = 'users'
@@ -6,6 +6,10 @@ app_name = 'users'
 urlpatterns = [
     path('register', views.register_user, name='register'),
     path('login/', views.login_user, name='log_in'),
+    path('logout', views.user_logout, name='logout'),
     path('perfil/', views.mi_perfil, name='mi_perfil'),
     path('configuracion/', views.configuracion, name='configuracion'),
+    path('delete_account/', views.delete_account, name='delete_account'),
+    path('change_password/', views.change_password, name='change_password'),
+    path('', include('movies.urls')),
 ]
